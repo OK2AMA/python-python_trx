2071dc35f7e2e3a59327b001a7fa4f93a00271b0
OK2AMA <jakubhoferek@gmail.com>
2016-10-30 15:06:40 +0100

    play and rec .. working on together with TRX

--- pyaudio_rec_ex.py
+++ pyaudio_rec_ex.py
@@ -1,7 +1,5 @@
-# source http://people.csail.mit.edu/hubert/pyaudio/ 
 import pyaudio
 import wave
-import sys
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -38,29 +36,3 @@ wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
 wf.writeframes(b''.join(frames))
 wf.close()
-
-"""PyAudio Example: Play a WAVE file.
-*********************************************************************
-"""
-
-
-
-wf = wave.open("output.wav", 'rb')
-
-p = pyaudio.PyAudio()
-
-stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                channels=wf.getnchannels(),
-                rate=wf.getframerate(),
-                output=True)
-
-data = wf.readframes(CHUNK)
-
-while data != '':
-    stream.write(data)
-    data = wf.readframes(CHUNK)
-
-stream.stop_stream()
-stream.close()
-
-p.terminate()
